de435806b4c29ca4ac4c2251c54db0dbd4fd3476
mcagabe19 <egzozu.be2.bas@gmail.com>
2023-01-29 22:32:27 +0300

    pt2

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -23,13 +23,8 @@ class StoryMenuState extends MusicBeatState
 	var scoreText:FlxText;
 
 	var weekData:Array<Dynamic> = [
-		['Tutorial'],
-		['Bopeebo', 'Fresh', 'Dad Battle'],
-		['Spookeez', 'South', "Monster"],
-		['Pico', 'Philly Nice', "Blammed"],
-		['Satin Panties', "High", "Milf"],
-		['Cocoa', 'Eggnog', 'Winter Horrorland'],
-		['Senpai', 'Roses', 'Thorns']
+		['Stevtorial'],
+		['hello', 'i can has diamonds', 'vibing', 'spammo'],
 	];
 	var curDifficulty:Int = 1;
 
@@ -38,21 +33,11 @@ class StoryMenuState extends MusicBeatState
 	var weekCharacters:Array<Dynamic> = [
 		['', 'bf', 'gf'],
 		['dad', 'bf', 'gf'],
-		['spooky', 'bf', 'gf'],
-		['pico', 'bf', 'gf'],
-		['mom', 'bf', 'gf'],
-		['parents-christmas', 'bf', 'gf'],
-		['senpai', 'bf', 'gf']
 	];
 
 	var weekNames:Array<String> = [
-		"",
-		"Daddy Dearest",
-		"Spooky Month",
-		"PICO",
-		"MOMMY MUST MURDER",
-		"RED SNOW",
-		"Hating Simulator ft. Moawling"
+		"how to fuck *funk* with stev",
+		"stev: the god of spamming",
 	];
 
 	var txtWeekTitle:FlxText;
