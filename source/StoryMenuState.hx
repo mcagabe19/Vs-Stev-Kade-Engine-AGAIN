03a57fe08e45eb41f2e22807b5ade836a92f06cb
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2023-01-30 17:07:02 +0300

    Update StoryMenuState.hx

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -56,7 +56,7 @@ class StoryMenuState extends MusicBeatState
 	var leftArrow:FlxSprite;
 	var rightArrow:FlxSprite;
 
-        var yellowBG:FlxSprite = new FlxSprite(0, 70).loadGraphic(Paths.image('menu_blank'));
+        var yellowBG:FlxSprite = new FlxSprite(0, 70).loadGraphic(Paths.image('menu_blank','stev'));
 
 	override function create()
 	{
@@ -346,9 +346,9 @@ class StoryMenuState extends MusicBeatState
 		switch (curWeek)
 		{
 		case 0:
-		yellowBG = new FlxSprite(0, 70).loadGraphic(Paths.image('menu_blank'));
+		yellowBG = new FlxSprite(0, 70).loadGraphic(Paths.image('menu_blank','stev'));
 		case 1:
-		yellowBG = new FlxSprite(0, 70).loadGraphic(Paths.image('menu_stevvvv'));
+		yellowBG = new FlxSprite(0, 70).loadGraphic(Paths.image('menu_stevvvv','stev'));
 		}
 
 		if (curWeek >= weekData.length)
