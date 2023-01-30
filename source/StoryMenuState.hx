f0651a4ea6737401d69ed6ea921d54df1150bd21
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2023-01-30 11:43:17 +0300

    Update StoryMenuState.hx

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -56,8 +56,6 @@ class StoryMenuState extends MusicBeatState
 	var leftArrow:FlxSprite;
 	var rightArrow:FlxSprite;
 
-        var yellowBG:FlxSprite = null;
-
 	override function create()
 	{
 		Paths.clearUnusedMemory();
@@ -93,7 +91,7 @@ class StoryMenuState extends MusicBeatState
 		rankText.screenCenter(X);
 
 		var ui_tex = Paths.getSparrowAtlas('campaign_menu_UI_assets');
-		yellowBG = new FlxSprite(0, 0).loadGraphic(Paths.image('menubg/blank'));
+		public var yellowBG:FlxSprite = new FlxSprite(0, 0).loadGraphic(Paths.image('menubg/blank'));
 
 		grpWeekText = new FlxTypedGroup<MenuItem>();
 		add(grpWeekText);
