6792065e78967f25cb18d6c7c61af884e885c09f
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2023-01-30 10:53:26 +0300

    Update StoryMenuState.hx

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -345,9 +345,9 @@ class StoryMenuState extends MusicBeatState
 		switch (curWeek)
 		{
 		case 0:
-		yellowBG:FlxSprite = new FlxSprite(0, 0).loadGraphic(Paths.image('menubg/blank'));
+		yellowBG = new FlxSprite(0, 0).loadGraphic(Paths.image('menubg/blank'));
 		case 1:
-		yellowBG:FlxSprite = new FlxSprite(0, 0).loadGraphic(Paths.image('menubg/stevvvv'));
+		yellowBG = new FlxSprite(0, 0).loadGraphic(Paths.image('menubg/stevvvv'));
 		}
 
 		if (curWeek >= weekData.length)
