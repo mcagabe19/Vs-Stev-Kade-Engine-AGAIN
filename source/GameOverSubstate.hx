001d6c436858e69dcc438b1828c4a519246cff36
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2023-01-30 12:03:12 +0300

    Update GameOverSubstate.hx

--- source/GameOverSubstate.hx
+++ source/GameOverSubstate.hx
@@ -26,6 +26,8 @@ class GameOverSubstate extends MusicBeatSubstate
 			case 'bf-pixel':
 				stageSuffix = '-pixel';
 				daBf = 'bf-pixel-dead';
+                        case 'bfbside':
+                                daBf = 'bfbside-dead';
 			default:
 				daBf = 'bf-dead';
 		}
